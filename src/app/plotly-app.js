b445fb5cbd19b1691b4118cb3896272eca109796
Tim <tgorsuch@redventures.com>
2022-08-09 10:50:17 -0400

    restructures folders

--- src/app/plotly-app.js
+++ src/app/plotly-app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import PartnerInjection from "./js/react/apps/partner-injection";
+import PartnerInjection from "../js/react/apps/partner-injection";
 
 const appContainer = document.querySelector("[data-app='plotly-app']");
 
